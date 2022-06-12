47e90936c6cd5635cbb7bbf60d5124318e404d5f
Mashkova Oksana <91336056+itsapumpkinpie@users.noreply.github.com>
2022-06-12 10:58:41 +0300

    This new commit Morrowind

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 # функция представления
 def index():
-    return "Живите и дальше в этом проклятом мире, который сами и создали"+"hey"
+    return "Живите и дальше в этом проклятом мире, который сами и создали"+"yee Morrowind is great game"
 
 if __name__ == "__name__":
     app.run(debug=True)
